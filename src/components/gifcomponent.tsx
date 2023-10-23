e9901d72268728bc45ecef7f5d30488668671be5
Montek <87750128+Montekkundan@users.noreply.github.com>
2023-10-23 00:43:33 -0700

    Update gifcomponent.tsx

--- src/components/gifcomponent.tsx
+++ src/components/gifcomponent.tsx
@@ -1,20 +1,50 @@
 import Image from "next/image";
+import { useState } from "react";
 
 type GifProps = {
-  src: string;
-  alt: string;
-};
-
-export function GifComponent({ src, alt }: GifProps) {
-        return (
-        <div className="flex justify-center p-5">
-            <Image
+    src: string;
+    alt: string;
+  };
+  
+  export function GifComponent({ src, alt }: GifProps) {
+    const [isPopup, setPopup] = useState(false);
+  
+    return (
+      <>
+        <div className="flex justify-center p-5" onClick={() => setPopup(true)}>
+          <Image
+            src={src}
+            alt={alt}
+            width={500}
+            height={500}
+            className="rounded-2xl"
+            loading="lazy"
+          />
+        </div>
+  
+        {isPopup && (
+          <div
+            className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50"
+            onClick={() => setPopup(false)}
+          >
+            <div className="relative">
+              <button
+                className="absolute top-0 right-0 p-2 bg-white"
+                onClick={() => setPopup(false)}
+              >
+                X
+              </button>
+              <Image
                 src={src}
                 alt={alt}
-                width={500}
-                height={500}
+                width={1000}
+                height={1000}
                 className="rounded-2xl"
-            />
-        </div>
+                loading="lazy"
+              />
+            </div>
+          </div>
+        )}
+      </>
     );
-        }
+  }
