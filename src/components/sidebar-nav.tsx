307e6050db3048f9e1171baccce9de26395eb694
Montek <87750128+Montekkundan@users.noreply.github.com>
2023-11-05 19:44:47 -0800

    feat: enhance sidebar navigation UI/UX
    This commit introduces several improvements to the sidebar navigation to enhance user experience:
    
    - Added background color change for active section titles for better visibility.
    - Implemented indent for child items within an open section for clearer hierarchy.
    - Included transition effects for chevron icons to indicate section open/close state.
    - Adjusted conditional rendering to toggle chevron icons depending on the section state.

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -5,6 +5,8 @@ import { usePathname } from "next/navigation"
 
 import { SidebarNavItem } from "@/types"
 import { cn } from "@/lib/utils"
+import { useEffect, useState } from "react"
+import { Icons } from "./icons"
 
 export interface DocsSidebarNavProps {
   items: SidebarNavItem[]
@@ -12,20 +14,61 @@ export interface DocsSidebarNavProps {
 
 export function DocsSidebarNav({ items }: DocsSidebarNavProps) {
   const pathname = usePathname()
+  const [openSections, setOpenSections] = useState({});
+  const toggleSection = (title: string) => {
+    setOpenSections(prev => ({
+      ...prev,
+      [title]: !prev[title],
+    }));
+  };
+
+  useEffect(() => {
+    const newOpenSections = {};
+  
+    // We find if a section contains the active item
+    let activeSectionTitle = '';
+    items.forEach((section) => {
+      const isActive = section.items.some(item => pathname === item.href);
+      if (isActive) {
+        activeSectionTitle = section.title;
+      }
+      newOpenSections[section.title] = isActive;
+    });
+  
+    // Ensuring only one section is open, the one that matches the active item
+    Object.keys(newOpenSections).forEach((title) => {
+      newOpenSections[title] = title === activeSectionTitle;
+    });
+  
+    setOpenSections(newOpenSections);
+  }, [pathname, items]);
+  
 
   return items.length ? (
     <div className="w-full">
-      {items.map((item, index) => (
-        <div key={index} className={cn("pb-8")}>
-          <h4 className="mb-1 rounded-md px-2 py-1 text-sm font-medium">
-            {item.title}
-          </h4>
-          {item.items ? (
+    {items.map((item, index) => (
+      <div key={index} className="pb-8">
+        <div  className={`mb-1 flex justify-between rounded-md px-2 py-1 text-sm font-medium cursor-pointer ${
+            openSections[item.title] ? 'bg-[#2b2f30]' : ''
+          }`}  onClick={() => toggleSection(item.title)} >
+        <h4>
+           
+          {item.title}
+          
+        </h4>
+        <Icons.chevronRight
+        className={`mr-2 h-4 w-4 transition-transform duration-300 ${openSections[item.title] ? 'transform rotate-90' : ''}`}
+      />
+            </div>
+        {openSections[item.title] ? (
+          // Add padding to the container for indentation
+          <div className="pl-4"> 
             <DocsSidebarNavItems items={item.items} pathname={pathname} />
-          ) : null}
-        </div>
-      ))}
-    </div>
+          </div>
+        ) : null}
+      </div>
+    ))}
+  </div>
   ) : null
 }
 
